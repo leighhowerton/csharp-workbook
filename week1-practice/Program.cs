784b3f1ed43379648702f5d1af141f0b099ccfdd
Leigh Howerton <leighhowerton@yahoo.com>
2018-10-08 18:51:57 -0500

    Steps 1-3

--- week1-practice/Program.cs
+++ week1-practice/Program.cs
@@ -1,12 +1,46 @@
 ﻿using System;
 
-namespace week1_practice
+public class Program
 {
-    class Program
-    {
-        static void Main(string[] args)
+
+        public static void Main()
         {
-            Console.WriteLine("Welcome to my cavern!");
-        }
-    }
+            // // Addition Assignment
+            // double num1 = 0;
+            // double num2 = 0;
+
+            // Console.WriteLine("Please enter a number: ");   
+            // num1 = Convert.ToInt32(Console.ReadLine());
+            // Console.WriteLine("Please enter a second number: ");
+            // num2 = Convert.ToInt32(Console.ReadLine());
+
+            // Console.WriteLine("Total: {0}", num1+num2);
+
+        //     // Yards to Inches
+        //     int yards = 0;
+        //     int inches;
+
+        //     Console.WriteLine("Please enter yards: ");
+        //     yards = Convert.ToInt32(Console.ReadLine());
+        //     inches = (yards * 12);
+
+        //     Console.WriteLine("{0} yards is eqaul to {1} inches", yards, inches);
+        // }
+
+        // Defining people as true
+        bool people = true;
+        bool snails = false;
+
+        Console.WriteLine("Who is faster? People or Snails?");
+        Console.ReadLine();
+
+            if (people)
+            {
+                Console.WriteLine("True! Yay for People!");
+            }
+            else (snails) 
+            {
+                Console.WriteLine("False! Snails are much slower.");
+            }
+     }
 }
