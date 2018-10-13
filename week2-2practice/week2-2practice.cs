d122cb9d88b2996a8b41bbec51bd3f25e799c3f6
Leigh Howerton <leighhowerton@yahoo.com>
2018-10-12 23:29:35 -0500

    week2-2practice

---
+++ week2-2practice/week2-2practice.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace week2_2practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int grade = 0;
+            Console.WriteLine("Hello! Please enter a number.");
+            String input = Console.ReadLine();
+            grade = Convert.ToInt32(input);
+
+            if(grade >= 90){
+                Console.WriteLine("You made an A!");
+            } else if (grade >= 80){
+                Console.WriteLine("You made a B!");
+            } else if (grade >= 70){
+                Console.WriteLine("You made a C!");
+            } else {
+                Console.WriteLine("You failed the exam.");
+            }
+
+
+
+            Console.WriteLine("That's all.");
+        }
+    }
+}
